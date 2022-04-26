5995e1699dd038de8ea7e6aa00da23284074aad9
shahriar.zaman <shahriar.zaman@randomaccessinc.com>
2022-04-25 20:03:51 -0400

    New Files added after fixing git error 404

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
+import { HeaderComponent } from './header/header.component';
 import { AppComponent } from './app.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { RecipesItemComponent } from './recipes/recipes-list/recipes-item/recipes-item.component';
+import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HeaderComponent,
+    AppComponent,
+    RecipesComponent,
+    RecipesListComponent,
+    RecipesItemComponent,
+    RecipesDetailComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css']
+})
+export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] =[
+    new Recipe('A test Recipe', 'This is Simply a Test', 
+    'https://thumbs.dreamstime.com/z/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg')
+  ];
+  
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
